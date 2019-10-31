87cfeb9bf558c000e02301ede39f161653720073
Thomas Reynolds <me@tdreyno.com>
2019-10-31 10:03:57 -0700

    Sub status

--- src/Subscription/Subscription.ts
+++ src/Subscription/Subscription.ts
@@ -1,23 +1,56 @@
 import { Task } from "../Task/Task";
 
-export type Subscriber<T> = (value: T) => void | Task<any, any>;
+export type Status = "active" | "inactive";
+export type EventSubscriber<T> = (value: T) => void | Task<any, any>;
+export type StatusSubscriber = (status: Status) => void;
 export type Unsubscriber = () => void;
 
 export class Subscription<T> {
-  private subscribers: Set<Subscriber<T>> = new Set<Subscriber<T>>();
+  private eventSubscribers: Set<EventSubscriber<T>> = new Set<
+    EventSubscriber<T>
+  >();
+  private status: Status = "inactive";
+  private statusSubscribers: Set<StatusSubscriber> = new Set<
+    StatusSubscriber
+  >();
 
   public emit(value: T): Task<any, any[]> {
     return Task.all(
-      Array.from(this.subscribers).map(sub => sub(value) || Task.empty())
+      Array.from(this.eventSubscribers).map(sub => sub(value) || Task.empty())
     );
   }
 
-  public subscribe(fn: Subscriber<T>): Unsubscriber {
-    this.subscribers.add(fn);
-    return () => this.subscribers.delete(fn);
+  public subscribe(fn: EventSubscriber<T>): Unsubscriber {
+    this.eventSubscribers.add(fn);
+
+    return () => {
+      this.eventSubscribers.delete(fn);
+
+      this.checkStatus();
+    };
   }
 
   public clear() {
-    this.subscribers.clear();
+    this.eventSubscribers.clear();
+
+    this.checkStatus();
+  }
+
+  public onStatusChange(fn: StatusSubscriber) {
+    this.statusSubscribers.add(fn);
+
+    return () => this.statusSubscribers.delete(fn);
+  }
+
+  private checkStatus() {
+    const newstatus = this.eventSubscribers.size > 0 ? "active" : "inactive";
+
+    if (newstatus === this.status) {
+      return;
+    }
+
+    this.status = newstatus;
+
+    Array.from(this.statusSubscribers).map(sub => sub(newstatus));
   }
 }
