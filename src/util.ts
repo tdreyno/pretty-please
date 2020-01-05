9f73139544372c594158a5d143fe8db7491dc37b
Thomas Reynolds <me@tdreyno.com>
2020-01-05 12:13:06 -0800

    mapToIndexedObject

--- src/util.ts
+++ src/util.ts
@@ -23,3 +23,15 @@ export function constant<T>(value: T): () => T {
 export function identity<T>(x: T): T {
   return x;
 }
+
+export function mapToIndexedObject<T, V, R extends { [key: string]: V }>(
+  fn: (item: T, index: number) => [string, V],
+  items: T[],
+  initial: R = {} as R
+): R {
+  return items.reduce((sum, item, index) => {
+    const [key, value] = fn(item, index);
+    (sum as { [key: string]: V })[key] = value;
+    return sum;
+  }, initial);
+}
